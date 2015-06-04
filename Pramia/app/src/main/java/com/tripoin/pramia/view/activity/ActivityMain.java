bdc05894b61b4eb2bc40d5d7aa0821836d48a0fb
KNIGHTMASTER <KNIGHTMASTER@users.noreply.github.com>
2015-06-04 21:54:18 +0700

    +Fauzi ++ Initiate commit Pramia ++ Base Activity ++ Base Fragment

---
+++ Pramia/app/src/main/java/com/tripoin/pramia/view/activity/ActivityMain.java
@@ -0,0 +1,43 @@
+package com.tripoin.pramia.view.activity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import br.liveo.interfaces.NavigationLiveoListener;
+import br.liveo.navigationliveo.NavigationLiveo;
+
+
+public class ActivityMain extends NavigationLiveo implements NavigationLiveoListener {
+
+
+    @Override
+    public void onUserInformation() {
+
+    }
+
+    @Override
+    public void onInt(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onItemClickNavigation(int i, int i2) {
+
+    }
+
+    @Override
+    public void onPrepareOptionsMenuNavigation(Menu menu, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onClickFooterItemNavigation(View view) {
+
+    }
+
+    @Override
+    public void onClickUserPhotoNavigation(View view) {
+
+    }
+}
